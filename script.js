56cf6a69d6ccfb0365dc412c76438c6ea1b26808
Hertanto Irawan <hertanto.irawan@outlook.com>
2021-11-22 03:14:13 +0700

    Make High Card game work

--- script.js
+++ script.js
@@ -1,1 +1,189 @@
-// Please implement exercise logic here
+/**
+ * Create a card element.
+ * @param {*} cardInfo Metadata of a card.
+ * @returns Card element.
+ */
+const createCard = (cardInfo) => {
+  const name = document.createElement('div');
+  name.classList.add('name', cardInfo.colour);
+  name.innerText = cardInfo.displayName;
+
+  const suit = document.createElement('div');
+  suit.classList.add('suit', cardInfo.colour);
+  suit.innerText = cardInfo.suitSymbol;
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.appendChild(name);
+  card.appendChild(suit);
+
+  return card;
+};
+
+const cardContainer = document.createElement('div');
+cardContainer.classList.add('card-container');
+document.body.appendChild(cardContainer);
+
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+const getRandomIndex = (max) => Math.floor(Math.random() * max);
+
+// Shuffle an array of cards
+const shuffleCards = (cards) => {
+  // Loop over the card deck array once
+  for (let currentIndex = 0; currentIndex < cards.length; currentIndex += 1) {
+    // Select a random index in the deck
+    const randomIndex = getRandomIndex(cards.length);
+    // Select the card that corresponds to randomIndex
+    const randomCard = cards[randomIndex];
+    // Select the card that corresponds to currentIndex
+    const currentCard = cards[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cards[currentIndex] = randomCard;
+    cards[randomIndex] = currentCard;
+  }
+  // Return the shuffled deck
+  return cards;
+};
+
+/**
+ * Make a new card deck.
+ * @returns An array of cards.
+ */
+const makeDeck = () => {
+  // Initialise an empty deck array
+  const newDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+  const suitsSymbol = ['♥️', '♦️', '♣️', '♠️'];
+
+  // Loop over the suits array
+  for (let suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    for (let rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
+      // By default, the card name is the same as rankCounter
+      let cardName = `${rankCounter}`;
+      let cardDisplayName = `${rankCounter}`;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName === '1') {
+        cardName = 'ace';
+        cardDisplayName = 'A';
+      } else if (cardName === '11') {
+        cardName = 'jack';
+        cardDisplayName = 'J';
+      } else if (cardName === '12') {
+        cardName = 'queen';
+        cardDisplayName = 'Q';
+      } else if (cardName === '13') {
+        cardName = 'king';
+        cardDisplayName = 'K';
+      }
+
+      let cardColour = 'black';
+      if ((suits[suitIndex] === 'hearts') || (suits[suitIndex] === 'diamonds')) {
+        cardColour = 'red';
+      }
+
+      // Create a new card info with the suit symbol, suit, name,
+      // display name, colour, and rank.
+      // -  suitSymbol: '♦️',
+      // -  suit: 'diamond',
+      // -  name: 'queen',
+      // -  displayName: 'Q',
+      // -  colour: 'red',
+      // -  rank: 12,
+      const card = {
+        suitSymbol: suitsSymbol[suitIndex],
+        suit: suits[suitIndex],
+        name: cardName,
+        displayName: cardDisplayName,
+        colour: cardColour,
+        rank: (rankCounter === 1) ? 14 : rankCounter,
+      };
+
+      // Add the new card to the deck
+      newDeck.push(card);
+    }
+  }
+
+  // Return the completed card deck
+  return newDeck;
+};
+
+const deck = shuffleCards(makeDeck());
+
+let playersTurn = 1; // matches with starting instructions
+let player1Card;
+
+const player1Button = document.createElement('button');
+const player2Button = document.createElement('button');
+
+const gameInfo = document.createElement('div');
+
+// Create a helper function for output to abstract complexity
+// of DOM manipulation away from game logic
+const output = (message) => {
+  gameInfo.innerText = message;
+};
+
+const player1Click = () => {
+  if (playersTurn === 1) {
+    // Pop player 1's card metadata from the deck
+    player1Card = deck.pop();
+
+    // Create card element from card metadata
+    const cardElement = createCard(player1Card);
+    // Empty cardContainer in case this is not the 1st round of gameplay
+    cardContainer.innerHTML = '';
+    // Append the card element to the card container
+    cardContainer.appendChild(cardElement);
+
+    // Switch to player 2's turn
+    playersTurn = 2;
+  }
+};
+
+const player2Click = () => {
+  if (playersTurn === 2) {
+    // Pop player 2's card metadata from the deck
+    const player2Card = deck.pop();
+
+    // Create card element from card metadata
+    const cardElement = createCard(player2Card);
+    // Append card element to card container
+    cardContainer.appendChild(cardElement);
+
+    // Switch to player 1's turn
+    playersTurn = 1;
+
+    // Determine and output winner
+    if (player1Card.rank > player2Card.rank) {
+      output('Player 1 wins!');
+    } else if (player1Card.rank < player2Card.rank) {
+      output('Player 2 wins!');
+    } else {
+      output('Tie!');
+    }
+  }
+};
+
+const initGame = () => {
+  // fill game info div with starting instructions
+  gameInfo.innerText = 'It\'s player 1 turn. Click to draw a card!';
+  document.body.appendChild(gameInfo);
+
+  // initialize button functionality
+  player1Button.innerText = 'Player 1 Draw';
+  document.body.appendChild(player1Button);
+
+  player2Button.innerText = 'Player 2 Draw';
+  document.body.appendChild(player2Button);
+
+  player1Button.addEventListener('click', player1Click);
+  player2Button.addEventListener('click', player2Click);
+};
+
+initGame();
