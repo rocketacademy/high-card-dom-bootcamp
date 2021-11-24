98e245cfe7005d95d6a6ef92ad5c96a90ca810cd
Hertanto Irawan <hertanto.irawan@outlook.com>
2021-11-25 01:17:28 +0700

    Add temporary console logs

--- script.js
+++ script.js
@@ -182,9 +182,11 @@ const arrangeCardsDisplay = (playerNum) => {
     if (player1Cards.length > 2) {
       sortedCards.sort(compareNumbers);
       player1CardList.innerHTML = '';
-
+      console.log('in 1 > 2', sortedCards);
       addCardOnList(1, sortedCards[0]);
+      console.log('in 1 > 2', sortedCards[0]);
       addCardOnList(1, sortedCards[sortedCards.length - 1]);
+      console.log('in 1 > 2', sortedCards[sortedCards.length - 1]);
 
       for (let i = 1; i < sortedCards.length - 1; i += 1) {
         addCardOnList(1, sortedCards[i]);
