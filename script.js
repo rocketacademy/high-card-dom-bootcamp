4b69f0e32cd872730420ef3a9019403a16673657
averygan <averygan19@gmail.com>
2021-11-25 23:02:14 +0800

    added draw multiple cards function

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
 
 //Global variables
 let playersTurn = 1; // matches with starting instructions
-let player1Card;
+let player1Card = [];
+let player2Card = [];
 const player1Button = document.createElement('button');
 const player2Button = document.createElement('button');
+// Button to compare cards
+const compareCardsButton = document.createElement(`button`);
 const gameInfo = document.createElement('div');
 let cardContainer;
 
@@ -13,30 +16,36 @@ cardContainer = document.createElement('div');
 cardContainer.classList.add('card-container');
 document.body.appendChild(cardContainer);
 
+// Player 2 card container
+let cardContainerTwo;
+cardContainerTwo = document.createElement('div');
+cardContainerTwo.classList.add(`card-container`);
+document.body.appendChild(cardContainerTwo);
+
 // Get a random index ranging from 0 (inclusive) to max (exclusive).
 const getRandomIndex = (max) => Math.floor(Math.random() * max);
 // Shuffle an array of cards
 const shuffleCards = (cards) => {
-  // Loop over the card deck array once
+  // Loop over the card  array once
   for (let currentIndex = 0; currentIndex < cards.length; currentIndex += 1) {
-    // Select a random index in the deck
+    // Select a random index in the 
     const randomIndex = getRandomIndex(cards.length);
     // Select the card that corresponds to randomIndex
     const randomCard = cards[randomIndex];
     // Select the card that corresponds to currentIndex
     const currentCard = cards[currentIndex];
-    // Swap positions of randomCard and currentCard in the deck
+    // Swap positions of randomCard and currentCard in the 
     cards[currentIndex] = randomCard;
     cards[randomIndex] = currentCard;
   }
-  // Return the shuffled deck
+  // Return the shuffled 
   return cards;
 };
 
-const makeDeck = () => {
-  // Initialise an empty deck array
+const make = () => {
+  // Initialise an empty  array
   const newDeck = [];
-  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  // Initialise an array of the 4 suits in our . We will loop over this array.
   const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
   const symbols = [`♥`, `◆`, `♣`, `♠`]
   // Loop over the suits array
@@ -71,12 +80,12 @@ const makeDeck = () => {
       newDeck.push(card);
     }
   }
-  // Return the completed card deck
+  // Return the completed card 
   return newDeck;
 };
 
-//Global variable to make and shuffle deck
-const deck = shuffleCards(makeDeck());
+//Global variable to make and shuffle 
+const deck = shuffleCards(make());
 // Create a helper function for output to abstract complexity
 // of DOM manipulation away from game logic
 const output = (message) => {
@@ -104,34 +113,33 @@ const createCard = (cardInfo) => {
 
 const player1Click = () => {
   if (playersTurn === 1) {
-    // Pop player 1's card metadata from the deck
-    player1Card = deck.pop();
-    
+    // Pop player 1's card metadata from the 
+    player1Card.push(deck.pop());
+    cardContainer.innerHTML = ``;
+    for (i = 0; i < player1Card.length; i++) {
     // Create card element from card metadata
-    const cardElement = createCard(player1Card);
-    // Empty cardContainer in case this is not the 1st round of gameplay
-    cardContainer.innerHTML = '';
+    const cardElement = createCard(player1Card[i]);
     // Append the card element to the card container
     cardContainer.appendChild(cardElement);
-    
-    // Switch to player 2's turn
-    playersTurn = 2;
+    }
   }
 };
 
 const player2Click = () => {
+  playersTurn = 2;
   if (playersTurn === 2) {
-    // Pop player 2's card metadata from the deck
-    const player2Card = deck.pop();
-    
-    // Create card element from card metadata
-    const cardElement = createCard(player2Card);    
-    // Append card element to card container
-    cardContainer.appendChild(cardElement);
-    
-    // Switch to player 1's turn
-    playersTurn = 1;
-    
+      player2Card.push(deck.pop());
+      cardContainerTwo.innerHTML = ``;
+      for (i = 0; i < player2Card.length; i++) {
+      // Create card element from card metadata
+      const cardElement = createCard(player2Card[i]);    
+      // Append card element to card container
+      cardContainerTwo.appendChild(cardElement);
+      }
+    }
+};
+
+const compareCards = () => {
     // Determine and output winner
     if (player1Card.rank > player2Card.rank) {
       output('player 1 wins');
@@ -141,7 +149,6 @@ const player2Click = () => {
       output('tie');
     }
   }
-};
 
 // Game initialisation 
 const initGame = () => {
@@ -149,9 +156,13 @@ const initGame = () => {
   player1Button.innerText = 'Player 1 Draw';
   document.body.appendChild(player1Button);
   player2Button.innerText = 'Player 2 Draw';
+  // Button to play after both players draw
   document.body.appendChild(player2Button);
+  compareCardsButton.innerText = `Play`;
+  document.body.appendChild(compareCardsButton);
   player1Button.addEventListener('click', player1Click);
   player2Button.addEventListener('click', player2Click);
+  compareCardsButton.addEventListener(`click`, compareCards);
   // fill game info div with starting instructions
   gameInfo.innerText = 'Its player 1 turn. Click to draw a card!';
   document.body.appendChild(gameInfo);
