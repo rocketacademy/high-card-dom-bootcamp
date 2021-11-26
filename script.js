704b5c27b1493079fe25e30b9fc2339995ff43a7
Shazwi <supershazwi@gmail.com>
2021-11-27 01:14:41 +0800

    finished more comfortable

--- script.js
+++ script.js
@@ -21,59 +21,59 @@ const player1Click = () => {
   // Create card element from card metadata
 
   if (numberOfCardsDrawn[0] !== numberOfCardsAllowed) {
+    // Create card element from card metadata
     const card = deck.pop();
     const cardElement = createCard(card);
     player1CardRanks.push(card.rank);
-    player1CardObjects.push(card);
-    player1MinMaxCards = checkMinMaxCards(player1CardRanks);
-
-    firstPlayerCardContainer.innerHTML = "";
-
-    if (player1CardObjects.length == 1) {
-      firstPlayerCardContainer.appendChild(cardElement);
-    } else {
-      firstPlayerCardContainer.appendChild(
-        createCard(player1CardObjects[player1MinMaxCards[0]])
-      );
-      firstPlayerCardContainer.appendChild(
-        createCard(player1CardObjects[player1MinMaxCards[1]])
-      );
-    }
-  } else {
+    // Empty cardContainer in case this is not the 1st round of gameplay
+    // Append the card element to the card container
+    firstPlayerCardContainer.appendChild(cardElement);
+    numberOfCardsDrawn[0]++;
     checkBothPlayersCards();
-  }
-
-  numberOfCardsDrawn[0]++;
+  } 
 };
 
 const player2Click = () => {
   // Create card element from card metadata
 
   if (numberOfCardsDrawn[1] !== numberOfCardsAllowed) {
+    // Create card element from card metadata
     const card = deck.pop();
     const cardElement = createCard(card);
     player2CardRanks.push(card.rank);
-    player2Cards.push(card);
-    player2MinMaxCards = checkMinMaxCards(player2CardRanks);
-
-    secondPlayerCardContainer.innerHTML = "";
-
-    if (player2Cards.length == 1) {
-      secondPlayerCardContainer.appendChild(cardElement);
-    } else {
-      secondPlayerCardContainer.appendChild(
-        createCard(player2Cards[player2MinMaxCards[0]])
-      );
-      secondPlayerCardContainer.appendChild(
-        createCard(player2Cards[player2MinMaxCards[1]])
-      );
-    }
-  } else {
+    // Empty cardContainer in case this is not the 1st round of gameplay
+    // Append the card element to the card container
+    secondPlayerCardContainer.appendChild(cardElement);
+    numberOfCardsDrawn[1]++;
     checkBothPlayersCards();
   }
 };
 
-const checkBothPlayersCards = () => {};
+const checkBothPlayersCards = () => {
+  if (numberOfCardsDrawn[0] == numberOfCardsAllowed && numberOfCardsDrawn[1] == numberOfCardsAllowed) {
+    calculateWinner();
+  }
+};
+
+const calculateWinner = () => {
+  // fill game info div with starting instructions
+  const player1Difference =
+    Math.max(...player1CardRanks) - Math.min(...player1CardRanks);
+  const player2Difference =
+    Math.max(...player2CardRanks) - Math.min(...player2CardRanks);
+
+  if (player1Difference > player2Difference) {
+    gameInfo.innerText = "Player 1 is the winner!";
+  } else if (player1Difference < player2Difference) {
+    gameInfo.innerText = "Player 2 is the winner!";
+  } else if (player1Difference == player2Difference) {
+    gameInfo.innerText = "Tie between players!";
+  }
+
+  gameInfo.innerText += ` | Player 1 Difference: ${player1Difference} | Player 2 Difference: ${player2Difference}`;
+
+  document.body.appendChild(gameInfo);
+};
 
 const initGame = () => {
   let numberOfCardsInput = document.createElement("input");
