ef4d4102cd0493660a2c2e41474019fd439b8b17
Noel Lim <noellimx@gmail.com>
2022-01-12 04:11:02 +0800

    -

--- script.js
+++ script.js
@@ -68,11 +68,6 @@ const CLASS_SEAT = `high-seat`;
 const CLASS_BANNER = `high-banner`;
 const CLASS_BUTTON_ROW = `high-button-row`;
 
-const ROOT_TAG = document.createElement(`div`);
-ROOT_TAG.classList.add();
-document.body.appendChild(ROOT_TAG);
-const PLAYER_NAMES = [`Player 1`, `Player 2`];
-
 const createPlayers = (names) => {
   return names.reduce((acc, name) => {
     return {
@@ -86,96 +81,106 @@ const createPlayers = (names) => {
     };
   }, {});
 };
-
-const CARDS = makeShuffledDeck();
-const PLAYERS = createPlayers(PLAYER_NAMES);
-const PLAYER_COUNT = PLAYER_NAMES.length;
-
 const drawCard = (cards) => cards.pop();
 
-const mat = document.createElement(`div`);
-mat.classList.add(CLASS_MAT);
+const main = (rootTag, playerNames) => {
+  const playerCount = playerNames.length;
+  const players = createPlayers(playerNames);
 
-const htmlSeatRow = document.createElement(`div`);
-htmlSeatRow.classList.add(CLASS_SEAT_ROW);
+  const cards = makeShuffledDeck();
 
-const htmlButtonRow = document.createElement(`div`);
-htmlButtonRow.classList.add(CLASS_BUTTON_ROW);
+  const mat = document.createElement(`div`);
+  mat.classList.add(CLASS_MAT);
 
-const htmlButtonRestart = document.createElement(`button`);
-const htmlButtonRestartDesc = document.createTextNode(`restart`);
-htmlButtonRestart.appendChild(htmlButtonRestartDesc);
+  const htmlSeatRow = document.createElement(`div`);
+  htmlSeatRow.classList.add(CLASS_SEAT_ROW);
 
-const htmlBanner = document.createElement(`div`);
-htmlBanner.classList.add(CLASS_BANNER);
+  const htmlButtonRow = document.createElement(`div`);
+  htmlButtonRow.classList.add(CLASS_BUTTON_ROW);
 
-const settle = (playerNames) => {
-  let maxNames = [];
-  let maxRank = -1;
+  const htmlButtonRestart = document.createElement(`button`);
+  htmlButtonRestart.addEventListener(`click`, () => main(rootTag, playerNames));
+  const htmlButtonRestartDesc = document.createTextNode(`restart`);
+  htmlButtonRestart.appendChild(htmlButtonRestartDesc);
+
+  const htmlBanner = document.createElement(`div`);
+  htmlBanner.classList.add(CLASS_BANNER);
+
+  const settle = (playerNames) => {
+    let maxNames = [];
+    let maxRank = -1;
+
+    for (const name of playerNames) {
+      const {
+        card: {
+          value: { rank },
+        },
+      } = players[name];
+      console.log(rank);
+      if (rank > maxRank) {
+        maxRank = rank;
+        maxNames = [name];
+      } else if (rank === maxRank) {
+        maxNames.push(name);
+      }
+    }
+    htmlBanner.innerText = `Winner${maxNames > 1 ? `s` : ``}: ${maxNames}`;
+    htmlButtonRow.replaceChildren(htmlButtonRestart);
+  };
+
+  let drawCount = 0;
+  const drawButtonPressed = (playerName) => {
+    console.log(`${playerName} pressed draw`);
+    const { card } = players[playerName];
+
+    if (card.value || card.html) {
+      !card.value && !card.html;
+      htmlBanner.innerText = `Hi ${playerName}, You've already drawn a card . . . `;
+      return;
+    } else {
+      const { seat } = players[playerName];
+      !!seat.html;
+      const cardValue = drawCard(cards);
+      card.html = document.createElement(`div`);
+      card.value = cardValue;
+      const { suit, name: CardName } = card.value;
+      card.html.innerText = `${suit[0].toUpperCase()} | ${CardName}`;
+      seat.html.appendChild(card.html);
+      drawCount += 1;
+      console.log(`draw c ${drawCount} plc ${playerCount}`);
+      if (drawCount === playerCount) {
+        settle(playerNames);
+      }
+    }
+  };
 
   for (const name of playerNames) {
-    const {
-      card: {
-        value: { rank },
-      },
-    } = PLAYERS[name];
-    console.log(rank);
-    if (rank > maxRank) {
-      maxRank = rank;
-      maxNames = [name];
-    } else if (rank === maxRank) {
-      maxNames.push(name);
-    }
+    const htmlSeat = document.createElement(`div`);
+    htmlSeat.classList.add(CLASS_SEAT);
+    players[name].seat.html = htmlSeat;
+    htmlSeatRow.appendChild(htmlSeat);
+
+    const htmlDrawButton = document.createElement(`button`);
+    const htmlDrawDesc = document.createTextNode(`${name} Draw`);
+    htmlDrawButton.appendChild(htmlDrawDesc);
+    htmlDrawButton.addEventListener(`click`, () =>
+      drawButtonPressed(name, htmlBanner)
+    );
+    players[name].draw.html = htmlDrawButton;
+    htmlButtonRow.appendChild(htmlDrawButton);
   }
-  htmlBanner.innerText = `Winner${maxNames > 1 ? `s` : ``}: ${maxNames}`;
-  htmlButtonRow.replaceChildren(htmlButtonRestart);
+
+  mat.replaceChildren(htmlSeatRow, htmlBanner);
+  rootTag.replaceChildren(mat, htmlButtonRow);
 };
 
-let drawCount = 0;
-const drawButtonPressed = (playerName) => {
-  console.log(`${playerName} pressed draw`);
-  const { card } = PLAYERS[playerName];
+const ROOT_TAG = document.createElement(`div`);
+ROOT_TAG.classList.add();
+document.body.appendChild(ROOT_TAG);
 
-  if (card.value || card.html) {
-    !card.value && !card.html;
-    htmlBanner.innerText = `Hi ${playerName}, You've already drawn a card . . . `;
-    return;
-  } else {
-    const { seat } = PLAYERS[playerName];
-    !!seat.html;
-    const cardValue = drawCard(CARDS);
-    card.html = document.createElement(`div`);
-    card.value = cardValue;
-    const { suit, name: CardName } = card.value;
-    card.html.innerText = `${suit[0].toUpperCase()} | ${CardName}`;
-    seat.html.appendChild(card.html);
-    drawCount += 1;
-    console.log(`draw c ${drawCount} plc ${PLAYER_COUNT}`);
-    if (drawCount === PLAYER_COUNT) {
-      settle(PLAYER_NAMES);
-    }
-  }
-};
-for (const name of PLAYER_NAMES) {
-  const htmlSeat = document.createElement(`div`);
-  htmlSeat.classList.add(CLASS_SEAT);
-  PLAYERS[name].seat.html = htmlSeat;
-  htmlSeatRow.appendChild(htmlSeat);
+const PLAYER_NAMES = [`Player 1`, `Player 2`];
 
-  const htmlDrawButton = document.createElement(`button`);
-  const htmlDrawDesc = document.createTextNode(`${name} Draw`);
-  htmlDrawButton.appendChild(htmlDrawDesc);
-  htmlDrawButton.addEventListener(`click`, () =>
-    drawButtonPressed(name, htmlBanner)
-  );
-  PLAYERS[name].draw.html = htmlDrawButton;
-  htmlButtonRow.appendChild(htmlDrawButton);
-}
-
-mat.appendChild(htmlSeatRow);
-mat.appendChild(htmlBanner);
-ROOT_TAG.appendChild(mat);
-ROOT_TAG.appendChild(htmlButtonRow);
+main(ROOT_TAG, PLAYER_NAMES);
 
 // Flow
 
